13443f8bf7e13478aa18f0c58e1c259e4fb03970
johnnieskywalker <lwolski0@gmail.com>
2023-02-26 18:23:26 +0100

    added functions with more meaningful names

--- dpk.js
+++ dpk.js
@@ -6,19 +6,23 @@ exports.deterministicPartitionKey = (event) => {
     return TRIVIAL_PARTITION_KEY;
   }
 
-  let candidate = initCandidate();
+  let candidate = getPartitionKeyCandidate(event);
 
   if (typeof candidate !== "string") {
     candidate = JSON.stringify(candidate);
   }
 
   if (candidate.length > MAX_PARTITION_KEY_LENGTH) {
-    candidate = crypto.createHash('sha3-512').update(candidate).digest('hex');
+    candidate = hashPartitionKey(candidate);
   }
 
   return candidate;
 
-  function initCandidate() {
+  function getPartitionKeyCandidate(event) {
     return event.partitionKey || crypto.createHash('sha3-512').update(JSON.stringify(event)).digest('hex');
+  }
+
+  function hashPartitionKey(partitionKey) {
+    return crypto.createHash('sha3-512').update(partitionKey).digest('hex');
   }
 };
